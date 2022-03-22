64ddc56f96d92d262fdd1a85f242cde45cc30b74
xichuanwushao <avatarwx@163.com>
2022-03-22 15:08:49 +0800

    1.2 将student-api服务注册项目到zookeeper

--- student-api/src/main/java/com/xichuan/stu1/Stu1Application.java
+++ student-api/src/main/java/com/xichuan/stu1/Stu1Application.java
@@ -2,7 +2,9 @@ package com.xichuan.stu1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient//用于注册服务到zookeeper或者console
 @SpringBootApplication
 public class Stu1Application {
 
