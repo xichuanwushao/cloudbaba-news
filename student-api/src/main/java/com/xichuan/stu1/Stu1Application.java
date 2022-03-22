fe5fd37ef6750dc08563b2a8b43de08706567217
xichuanwushao <avatarwx@163.com>
2022-03-22 13:53:43 +0800

    1.新增2个模块 2.加入cloudAlibaba依赖

---
+++ student-api/src/main/java/com/xichuan/stu1/Stu1Application.java
@@ -0,0 +1,13 @@
+package com.xichuan.stu1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stu1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stu1Application.class, args);
+    }
+
+}
---
+++ teacher-api/src/main/java/com/xichuan/tea1/Tea1Application.java
@@ -0,0 +1,13 @@
+package com.xichuan.tea1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tea1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tea1Application.class, args);
+    }
+
+}
