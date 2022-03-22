5cf6a8292bda8acf856e738849d53e8911a5305c
xichuanwushao <avatarwx@163.com>
2022-03-22 15:44:07 +0800

    1.2 使用zookeeper进行服务调用 teacher调用student的方法

--- teacher-api/src/main/java/com/xichuan/tea1/Tea1Application.java
+++ teacher-api/src/main/java/com/xichuan/tea1/Tea1Application.java
@@ -2,7 +2,9 @@ package com.xichuan.tea1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient//用于注册服务到zookeeper或者console
 @SpringBootApplication
 public class Tea1Application {
 
--- teacher-api/src/main/java/com/xichuan/tea1/controller/HelloController.java
+++ teacher-api/src/main/java/com/xichuan/tea1/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.xichuan.tea1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("teacher")
 public class HelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/hello")
     public Object hello() {
@@ -17,7 +25,13 @@ public class HelloController {
 
     @GetMapping("/myStuList")
     public Object myStuList() {
-        return "hello teacher ~~";
+
+        String url = "http://service-student";
+
+        List list = restTemplate.getForObject(url + "/service-student/student/getStuListByTeacherId?teacherId=10011", ArrayList.class);
+        //注意这里加了 context-path 所以路径路由多了一个service-student
+
+        return list;
     }
 
 
