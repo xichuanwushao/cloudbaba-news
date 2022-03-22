d12d7559beb23c6ba38e82e49d67ab4fe4b30b26
xichuanwushao <avatarwx@163.com>
2022-03-22 12:20:54 +0800

    1.初始化项目

---
+++ src/test/java/com/xichuan/news/CloudbabaNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xichuan.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbabaNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
