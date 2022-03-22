9f1bd13849ae019a2b7614b43740d1bd37522a39
xichuanwushao <avatarwx@163.com>
2022-03-22 16:01:54 +0800

    1.4 新增gateway模块

---
+++ gateway/src/main/java/com/xichuan/gy/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.xichuan.gy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient//用于注册服务到zookeeper或者console
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("http://localhost:8001/service-gateway/gateway/hello");
+    }
+
+}
