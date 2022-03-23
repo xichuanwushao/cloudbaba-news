c801c6fb1b232cc21f25e710d1b6279d0efd0c1f
xichuanwushao <avatarwx@163.com>
2022-03-23 12:01:43 +0800

    2.2.4 19-1 Nacos 实现动态配置管理 1.对于ID中的dev来说这个配置没有用 spring:   application:     name: service-teacher     profiles:       active: dev 2.这个配置才有用-Dspring.profiles.active=dev

--- teacher-api/src/main/java/com/xichuan/tea1/controller/HelloController.java
+++ teacher-api/src/main/java/com/xichuan/tea1/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.xichuan.tea1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("teacher")
+@RefreshScope       // 动态刷新的注解
 public class HelloController {
+
+    @Value("${alibaba.teacher.name}")
+    private String name;
+
+    @Value("${alibaba.teacher.age}")
+    private Integer age;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -42,6 +51,11 @@ public class HelloController {
         //注意这里加了 context-path 所以路径路由多了一个service-student
 
         return list;
+    }
+
+    @GetMapping("/info")
+    public Object info() {
+        return name + " - " + age;
     }
 
 
